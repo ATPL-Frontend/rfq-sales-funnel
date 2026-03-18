d6b1fe8542c10da9a34e254ddfe5904f31997f0d
imranhei <imranhossainrana.2014@gmail.com>
2026-03-18 14:13:46 +0600

    update email.js

--- server/utils/email.js
+++ server/utils/email.js
@@ -5,18 +5,26 @@ export const sendMail = async (to, subject, html) => {
   const transporter = nodemailer.createTransport({
     host: process.env.EMAIL_HOST,
     port: Number(process.env.EMAIL_PORT),
-    secure: true,
+    secure: false, // correct for 587
     auth: {
       user: process.env.EMAIL_USER,
       pass: process.env.EMAIL_PASS,
     },
+    requireTLS: true,
     tls: {
       rejectUnauthorized: false,
     },
     family: 4,
+    connectionTimeout: 20000,
+    greetingTimeout: 20000,
+    socketTimeout: 30000,
+    logger: true,
+    debug: true,
   });
 
   try {
+    await transporter.verify();
+
     const info = await transporter.sendMail({
       from: `"RFQ login" <${process.env.EMAIL_USER}>`,
       to,
@@ -24,6 +32,7 @@ export const sendMail = async (to, subject, html) => {
       html,
     });
 
+    console.log("Mail sent:", info);
     return info;
   } catch (error) {
     console.error("❌ Error sending email:", error);
